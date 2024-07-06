86f6fbfb81cd9332031fe0f692861b3327efd252
linuztx <linuztx@gmail.com>
2024-07-06 15:49:31 +0800

    Major Update

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
 # Proot Server
 
-This repository contains a script for installing and running Ubuntu Server 22.04 on servers that do not have root access. The script uses PRoot, a user-space implementation of chroot, mount --bind, and binfmt_misc, allowing you to perform actions that normally require root access. This makes it possible to install a full-fledged Ubuntu Server 22.04 environment on servers where you don't have root privileges.
+This repository contains a script for installing and running a Linux base system (Ubuntu, Alpine, or Debian) on servers or environments that do not have root access. The script utilizes proot, a user-space implementation of chroot, allowing you to perform actions that typically require root access. This makes it possible to set up a full-fledged Linux environment on servers or within containers where you don't have root privileges.
 
 ## Prerequisites
 
-Before running the script, make sure the following tools are installed:
+Before running the script, ensure the following tools are installed:
 
 - curl: Used for fetching necessary files from the internet.
 - tar: Used for extracting the downloaded files.
-- supported architecture: The script is tested on x86_64 and aarch64 architectures. Other architectures may not work.
+- proot: Used for creating a chroot-like environment without root privileges.
+
+## Supported Architectures
+
+The script is tested on x86_64 and aarch64 architectures. Other architectures may not work as expected.
 
 ## Usage
 
-To use the script, simply run the `run.sh` file. The script will download the necessary files, create a new directory for the Ubuntu Server environment, and start a shell session inside the environment. You can then install additional packages, configure the environment, and run services as needed.
+To use the script, simply run the `run.sh` file. The script will download the necessary files, create a new directory for the chosen Linux environment, and start a shell session inside the environment. You can then install additional packages, configure the environment, and run services as needed.
 
 To exit the environment, simply type `exit` twice in the shell session. You can re-enter the environment by running the `run.sh` script again.
 
 ## Limitations
 
-While PRoot provides a user-space implementation of chroot, it does not provide full isolation like a true chroot environment. This means that some actions that require root access may not work as expected. For example, you may not be able to create network interfaces, modify system files, or perform other administrative tasks that require root privileges.
+While proot provides a user-space implementation of chroot, it does not provide full isolation like a true chroot environment. This means that some actions that require root access may not work as expected. For example, you may not be able to create network interfaces, modify system files, or perform other administrative tasks that require root privileges.
 
 ## Installation
 
@@ -27,7 +31,6 @@ To use this script, you need to clone the repository, navigate to the directory,
 ```shellscript
 git clone https://github.com/linuztx/proot-server.git
 cd proot-server
-chmod +x run.sh
 ./run.sh
 ```
 
--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
-### CREATE BY @Linuztx ###
-### UBUNTU 22.04 ###
+### CREATED BY @Linuztx ###
+### SERVER INSTALLER PRO ###
 ### 2024 ###
-# This script downloads and installs the Ubuntu Server 22.04 base system
-# and sets up a proot environment to run it.
+# This script downloads and installs a Linux base system (Ubuntu, Alpine, or Debian) 
+# and sets up a proot environment to run the chosen system
 
-# Constants
+# Constants for the script
 TIMEOUT=1
 MAX_RETRIES=10
+ARCH_DEFAULT=$(uname -m)
 
 # Determine the alternative architecture name based on the current architecture
 case "$(uname -m)" in
@@ -20,50 +21,162 @@ case "$(uname -m)" in
         ;;
 esac
 
-# Check if the Ubuntu base system is already installed
-if [ ! -d "$(pwd)/bin" ]; then
-    echo "=================================================="
-    echo "      @Linuztx Ubuntu Server 22.04 Installer      "
-    echo "=================================================="
-    echo "                 @Copyright 2024                  "
-    echo "=================================================="
-    # Prompt the user for installation confirmation
-    while true; do
-        read -p 'Do you want to install Ubuntu Server 22.04? (Yes/no): ' prompt
-        case $prompt in
-            [yY][eE][sS])
-                echo -e "Starting installation...\n"
-                curl -L --retry $MAX_RETRIES --retry-delay $TIMEOUT --output /tmp/rootfs.tar.gz \
-                    "https://cdimage.ubuntu.com/ubuntu-base/releases/22.04/release/ubuntu-base-22.04-base-${ARCH_ALT}.tar.gz"
-                if [ $? -eq 0 ]; then
-                    tar -xf /tmp/rootfs.tar.gz -C "$(pwd)"
-                    rm -f /tmp/rootfs.tar.gz
-                    echo -e "\nUbuntu Server 22.04 base system installed successfully.\n"
-                else
-                    echo -e "\nFailed to download the Ubuntu Server 22.04 base system.\n"
+echo "=================================================="
+echo "|      @Linuztx Linux Server Installer Pro       |"
+echo "=================================================="
+echo "|                @Copyright 2024                 |"
+echo "=================================================="
+echo "|        1.) Ubuntu (24.04, Noble Numbat)        |"
+echo "|        2.) Alpine (3.19, Linux)                |"
+echo "|        3.) Debian (12, Bookworm)               |"
+echo "|        4.) Use the already installed distro    |"
+echo "=================================================="
+read -p "Choose a distro (1/2/3/4): " distro
+while true; do
+    case $distro in
+    1) 
+        distro_dir="ubuntu"
+        while true; do
+            read -p 'Do you want to install Ubuntu Server 24.04, Noble Numbat, Linux? (Yes/no): ' prompt
+            case $prompt in
+                [yY][eE][sS])
+                    echo -e "Starting installation...\n"
+                    curl -L --retry $MAX_RETRIES --retry-delay $TIMEOUT --output /tmp/rootfs.tar.xz \
+                        "https://github.com/termux/proot-distro/releases/download/v4.11.0/ubuntu-noble-${ARCH_DEFAULT}-pd-v4.11.0.tar.xz"
+                    if [ $? -eq 0 ]; then
+                        mkdir $(pwd)/$distro_dir
+                        tar -xf /tmp/rootfs.tar.xz -C "$(pwd)/$distro_dir" --strip-components=1
+                        rm -f /tmp/rootfs.tar.xz
+                        echo -e "\nUbuntu Server 24.04, Noble Numbat Linux base system installed successfully.\n"
+                    else
+                        echo -e "\nFailed to download the Ubuntu Server 24.04, Noble Numbat, Linux base system.\n"
+                        exit 1
+                    fi
+                    break 2
+                    ;;
+                [nN][oO])
+                    echo -e "Skipping Installation...\n"
+                    break 2
+                    ;;
+                *)
+                    echo "Please answer Yes or no."
+                    ;;
+            esac
+        done
+    ;;
+    2)
+        distro_dir="alpine"
+        while true; do
+            read -p 'Do you want to install Alpine Server 3.19, Linux? (Yes/no): ' prompt
+            case $prompt in
+                [yY][eE][sS])
+                    echo -e "Starting installation...\n"
+                    curl -L --retry $MAX_RETRIES --retry-delay $TIMEOUT --output /tmp/rootfs.tar.gz \
+                        "https://dl-cdn.alpinelinux.org/alpine/v3.19/releases/x86_64/alpine-minirootfs-3.19.1-${ARCH_DEFAULT}.tar.gz"
+                    if [ $? -eq 0 ]; then
+                        mkdir $(pwd)/$distro_dir
+                        tar -xf /tmp/rootfs.tar.gz -C "$(pwd)/$distro_dir"
+                        rm -f /tmp/rootfs.tar.gz
+                        echo -e "\nAlpine Server 3.19 Linux base system installed successfully.\n"
+                    else
+                        echo -e "\nFailed to download the Alpine Server 3.19, Linux base system.\n"
+                        exit 1
+                    fi
+                    break 2
+                    ;;
+                [nN][oO])
+                    echo -e "Skipping Installation...\n"
+                    break 2
+                    ;;
+                *)
+                    echo "Please answer Yes or no."
+                    ;;
+            esac
+        done
+    ;;
+    3)        
+        distro_dir="debian"
+        while true; do
+            read -p 'Do you want to install Debian Server 12, bookworm? (Yes/no): ' prompt
+            case $prompt in
+                [yY][eE][sS])
+                    echo -e "Starting installation...\n"
+                    curl -L --retry $MAX_RETRIES --retry-delay $TIMEOUT --output /tmp/rootfs.tar.xz \
+                        "https://github.com/termux/proot-distro/releases/download/v4.7.0/debian-bullseye-${ARCH_DEFAULT}-pd-v4.7.0.tar.xz"
+                    if [ $? -eq 0 ]; then
+                        mkdir $(pwd)/$distro_dir
+                        tar -xf /tmp/rootfs.tar.xz -C "$(pwd)/$distro_dir" --strip-components=1
+                        rm -f /tmp/rootfs.tar.xz 
+                        echo -e "\nDebian Server 12, bookworm base system installed successfully.\n"
+                    else
+                        echo -e "\nFailed to download the Debian Server 12, bookworm base system.\n"
+                        exit 1
+                    fi
+                    break 2
+                    ;;
+                [nN][oO])
+                    echo -e "Skipping Installation...\n"
+                    break 2
+                    ;;
+                *)
+                    echo "Please answer Yes or no."
+                    ;;
+            esac
+        done
+    ;;
+    4)
+        if [ -d "$(pwd)/ubuntu" ] || [ -d "$(pwd)/alpine" ] || [ -d "$(pwd)/debian" ]; then
+            echo -e "\n=================================================="
+            echo "|       Available Installed Linux Distros        |"
+            echo "=================================================="
+            [ -d "$(pwd)/ubuntu" ] && echo "|        1.) Ubuntu (24.04, Noble Numbat)        |"
+            [ -d "$(pwd)/alpine" ] && echo "|        2.) Alpine (3.19, Linux)                |"
+            [ -d "$(pwd)/debian" ] && echo "|        3.) Debian (12, Bookworm)               |"
+            echo "=================================================="
+            read -p "Choose a distro to use (1/2/3): " installed_distro
+            case $installed_distro in
+                1)
+                    distro_dir="ubuntu"
+                    ;;
+                2)
+                    distro_dir="alpine"
+                    ;;
+                3)
+                    distro_dir="debian"
+                    ;;
+                *)
+                    echo -e "\nInvalid choice. Please choose 1, 2, or 3.\n"
                     exit 1
-                fi
-                break
-                ;;
-            [nN][oO])
-                echo -e "Skipping Installation...\n"
-                exit 0
-                ;;
-            *)
-                echo "Please answer Yes or no."
-                ;;
-        esac
-    done
-fi
+                    ;;
+            esac
+            
+            # Check if the chosen directory exists
+            if [ ! -d "$(pwd)/$distro_dir" ]; then
+                echo -e "\nThe directory for the chosen distro does not exist. Please install the distro first.\n"
+                exit 1
+            fi
+            echo -e "Using the already installed $distro_dir base system.\n"
+        else
+            echo -e "No distro installed. Please install a distro first."
+            exit 1
+        fi
+        break  # Exit the loop
+    ;;            
+    *)
+        echo -e "\nInvalid choice. Please choose 1, 2, or 3.\n"
+    ;;
+    esac
+done
+        
 
 # Download and set up proot if not already installed
-if [ ! -x "$(pwd)/usr/local/bin/proot" ]; then
-    mkdir -p "$(pwd)/usr/local/bin"
-    while [ ! -s "$(pwd)/usr/local/bin/proot" ]; do
-        rm -f "$(pwd)/usr/local/bin/proot"
-        curl -L --retry $MAX_RETRIES --retry-delay $TIMEOUT --output "$(pwd)/usr/local/bin/proot" "https://proot.gitlab.io/proot/bin/proot"
-        if [ -s "$(pwd)/usr/local/bin/proot" ]; then
-            chmod 755 "$(pwd)/usr/local/bin/proot"
+if [ ! -x "$(pwd)/$distro_dir/usr/local/bin/proot" ]; then
+    mkdir -p "$(pwd)/$distro_dir/usr/local/bin"
+    while [ ! -s "$(pwd)/$distro_dir/usr/local/bin/proot" ]; do
+        rm -f "$(pwd)/$distro_dir/usr/local/bin/proot"
+        curl -L --retry $MAX_RETRIES --retry-delay $TIMEOUT --output "$(pwd)/$distro_dir/usr/local/bin/proot" "https://proot.gitlab.io/proot/bin/proot"
+        if [ -s "$(pwd)/$distro_dir/usr/local/bin/proot" ]; then
+            chmod 755 "$(pwd)/$distro_dir/usr/local/bin/proot"
             echo -e "\nproot has been downloaded and permissions have been set successfully.\n"
         else
             echo -e "\nFailed to download proot, retrying in 5 seconds...\n"
@@ -73,22 +186,22 @@ if [ ! -x "$(pwd)/usr/local/bin/proot" ]; then
 fi
 
 # Finalize the installation if not already done
-if [ ! -f "$(pwd)/etc/resolv.conf" ]; then
-    mkdir -p "$(pwd)/etc"
-    printf "nameserver 1.1.1.1\nnameserver 1.0.0.1\n" > "$(pwd)/etc/resolv.conf"
+if [ ! -f "$(pwd)/$distro_dir/etc/resolv.conf" ]; then
+    mkdir -p "$(pwd)/$distro_dir/etc"
+    printf "nameserver 1.1.1.1\nnameserver 1.0.0.1\n" > "$(pwd)/$distro_dir/etc/resolv.conf"
 fi
 
 # Display the startup message
 echo "=================================================="
-echo "      @Linuztx Ubuntu Server 22.04 Installer      "
+echo "|       @Linuztx Linux Server Installer Pro      |"
 echo "=================================================="
-echo "                 @Copyright 2024                  "
+echo "|                @Copyright 2024                 |"
 echo "=================================================="
-echo "              Starting Ubuntu Server...           "
+echo "|            Starting Linux Server...            |"
 echo "=================================================="
 echo 'To login as root, type "su"'
 echo -e "To quit the proot environment, enter \"exit\" twice to fully exit.\n"
 
 # Start the Ubuntu environment using proot
-"$(pwd)/usr/local/bin/proot" --rootfs="$(pwd)" \
+"$(pwd)/$distro_dir/usr/local/bin/proot" --rootfs="$(pwd)/$distro_dir" \
     -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
